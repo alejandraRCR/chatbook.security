44eb8caf708aab1bd144a90392cff9df0478476c
Alejandra Rodriguez <jacfalcon.better.practices@gmail.com>
2025-07-22 02:32:28 -0600

    Ajustado en codigo limpio

--- src/main/java/com/chatboot/config/CorsConfig.java
+++ src/main/java/com/chatboot/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://127.0.0.1:5001"));
+        configuration.setAllowedOrigins(Arrays.asList("http://127.0.0.1:5001"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
